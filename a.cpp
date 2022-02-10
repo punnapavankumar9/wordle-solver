c004cfa10b0d76ace7b2f0917b342bdee176600c
pavan kumar punna <67155391+punnapavankumar9@users.noreply.github.com>
2022-02-10 20:41:26 +0530

    added guiding messages in terminal

--- a.cpp
+++ a.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#include <conio.h>
 using namespace std;
 const int MATCH = 0, EXISTS = 1, NOT_EXISTS = 2;
 vector<string> dict;
+
 
 struct Word{
     int a[5];
@@ -21,19 +23,18 @@ struct Word{
         return true;
     }
     void print(string guess){
-    for(int i = 0; i < 5; i++){
-        if(a[i] == MATCH){
-            cout << "\033[1;32m";
-        }else if(a[i] == EXISTS){
-            cout << "\033[1;33m";
-        }else{
-            cout << "\033[1;37m";
+        for(int i = 0; i < 5; i++){
+            if(a[i] == MATCH){
+                cout << "\033[1;32m";
+            }else if(a[i] == EXISTS){
+                cout << "\033[1;33m";
+            }else{
+                cout << "\033[1;37m";
+            }
+            cout << guess[i] << " " << "\033[1;37m";
         }
-        cout << guess[i] << " " << "\033[0m";
+        cout << "\n";
     }
-    cout << "\n";
-}
-
 };
 
 
@@ -67,20 +68,26 @@ Word compare(string hidden, string guess){
     }
     return word;
 }
+void styled_cout(string s){
+    cout << "Word: ";
+    for(const char ch:s){
+        cout << ch << " ";
+    }
+    cout << "\n";
+}
 
 int solve(){
     vector<string> matching_words = dict;
     int queries = 0;
     int i = 1;
-    
     while(true && matching_words.size()){
-        string s = "";
-        if(i++ != 1)
-            s = matching_words[rand() % matching_words.size()];
-        else
-            s = "awarn";
-        cout << s << "\n";
+        string s = matching_words[rand() % matching_words.size()];
+        styled_cout(s);
+        if(matching_words.size() == 1){
+            break;
+        }
         Word word;
+        cout << "wordle response: " ;
         for(int i = 0; i < 5; i++){
             cin >> word.a[i]; 
         }
@@ -98,6 +105,7 @@ int solve(){
             }
         }
         matching_words = new_matching_words;
+        
     }
     if(matching_words.size() == 1)
         cout << "congratulations! You Won!" << "\n";
@@ -110,8 +118,13 @@ int solve(){
 int main(){
     std::srand(std::time(0));
     load_data();
-    int avg = 0;
+    cout << "\033[1;37m";
+    cout << "Please enter below word in the wordle and input response\n";
+    cout << "For \033[1;32mGreen \033[1;37mColor enter 0\n";
+    cout << "For \033[1;33mYellow \033[1;37mColor enter 1\n";
+    cout << "For \033[1;0mGray \033[1;37mColor enter 2\n";
     int n = dict.size();
     solve();
+    cout << "\033[0m";
     return 0;
 }
